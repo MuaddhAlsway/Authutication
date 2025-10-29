0d9a8528f29287c4e38aaff645591aa81eb314ec
Muaddh Alsway <muaddhalsway@gmail.com>
2025-10-29 03:26:02 +0300

    Complete Seventh Stage | Middleware & Login 🔐🧱

--- auth-master/action/login.ts
+++ auth-master/action/login.ts
@@ -1,15 +1,27 @@
-'use server';
+"use client";
 
-import * as z from 'zod';
-import { revalidatePath, revalidateTag } from "next/cache";
+import { signIn } from 'next-auth/react';
 import { LoginScheme } from '@/schemas';
+import * as z from 'zod';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
-export const login = async (values: z.infer<typeof LoginScheme>) =>{
-    const validatedFields = LoginScheme.safeParse(values);
+export const login = async (values: z.infer<typeof LoginScheme>) => {
+  const validatedFields = LoginScheme.safeParse(values);
+  if (!validatedFields.success) return { error: 'Invalid fields!' };
 
-    if(!validatedFields.success){
-        return{error: 'Invalid fields!'};
-    }
+  const { email, password } = validatedFields.data;
 
-    return {success: 'Email sent!'}
-}
+  const res = await signIn('credentials', {
+    redirect: false,
+    email,
+    password,
+    callbackUrl: DEFAULT_LOGIN_REDIRECT,
+  });
+
+  if (!res || res.error) return { error: 'Invalid credentials!' };
+
+  // Redirect manually
+  if (res.url) window.location.href = res.url;
+
+  return { success: true };
+};
